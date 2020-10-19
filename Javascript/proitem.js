8b2f3e4ab83fd08164f8a594e7a41381f1040dc7
Kelompok3-WebProgramming <Afirnandius@gmail.com>
2020-10-20 03:57:16 +0700

    20 Oct

--- Javascript/proitem.js
+++ Javascript/proitem.js
@@ -1,33 +1,3 @@
-document.getElementById('baju').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('keti').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('mobil').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
 document.getElementById('momogi').addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'flex';
     document.querySelector('#demo-content').style.display = 'none';
@@ -178,10 +148,7 @@ document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('#demo-content').style.display = "block";
 });
 
-
-
-
-document.getElementById('bajuslide').addEventListener('click', function(){
+document.getElementById('baju').addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'flex';
     document.querySelector('#demo-content').style.display = 'none';
 });
@@ -191,7 +158,7 @@ document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('#demo-content').style.display = "block";
 });
 
-document.getElementById('samsungslide').addEventListener('click', function(){
+document.getElementById('keti').addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'flex';
     document.querySelector('#demo-content').style.display = 'none';
 });
@@ -201,130 +168,7 @@ document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('#demo-content').style.display = "block";
 });
 
-document.getElementById('airpodsslide').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-
-
-
-document.getElementById('baju2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('keti2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('mobil2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('masker2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('jeans2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('ig2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('kotak2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('spidol2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('beras2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('airpods2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('potabi2').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('#demo-content').style.display = 'none';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = "none";
-    document.querySelector('#demo-content').style.display = "block";
-});
-
-document.getElementById('japota2').addEventListener('click', function(){
+document.getElementById('mobil').addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'flex';
     document.querySelector('#demo-content').style.display = 'none';
 });
