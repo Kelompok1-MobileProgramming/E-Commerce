29abc29e71aabad299607aff3ff1d20d3eccc5cb
cynthianatalie535190034 <cynthia.535190034@stu.untar.ac.id>
2020-10-28 00:05:56 +0700

    28 oct 00:05

--- Javascript/search.js
+++ Javascript/search.js
@@ -2,7 +2,16 @@ function search_item() {
     let input = document.getElementById('searchbar').value 
     input=input.toLowerCase(); 
     let x = document.getElementsByClassName('items'); 
-      
+    if(input){
+        document.querySelector('h2').style.display = "none";
+        document.querySelector('.noitems').style.display = "none";
+        document.querySelector('.mySlide fade').style.display = "none";
+    }
+    else{
+        document.querySelector('h2').style.display = "block";
+        document.querySelector('.noitems').style.display = "block";
+    }
+
     for (i = 0; i < x.length; i++) {  
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
             x[i].style.display="none"; 
