73ddd530ea14bdbe9a9b59cde4c9241bb538fcad
andri firnandius <afirnandius@gmail.com>
2020-11-02 10:48:41 +0700

    2 Nov 10:48

--- Javascript/search.js
+++ Javascript/search.js
@@ -5,28 +5,19 @@ function search_item() {
     ol = document.getElementById('list');
     let y = document.getElementsByClassName('noitems');
 
-    /*if(input.value.length == 0){
-        ul.style.display = "none";
-        return;
-    }else{
-        ul.style.display = "block";
-    }
     if(input){
-        document.querySelector('h2').style.display = "none";
-        document.querySelector('.noitems').style.display = "none";
-        document.querySelector('.mySlide fade').style.display = "none";
+        document.querySelector('.product').style.display = 'flex';
     }
     else{
-        document.querySelector('h2').style.display = "block";
-        document.querySelector('.noitems').style.display = "block";
-    }*/
+        document.querySelector('.product').style.display = 'none';
+    }
 
     for (i = 0; i < x.length; i++) {  
-        document.querySelector('.product').style.display = 'flex';
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
             x[i].style.display="none"; 
         } 
         else if (x[i].innerHTML.toLowerCase().includes(input)) { 
+            
             x[i].style.display="list-item";                  
         } 
         else{
@@ -38,48 +29,3 @@ function search_item() {
         y[e].style.display="none";
     }
 }
-
-/*
-const itemsList = document.getElementById('list');
-const searchBar = document.getElementById('searchbar');
-let items = [];
-
-searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-
-    const filteredItems = items.filter((items) => {
-        return (
-            items.name.toLowerCase().includes(searchString) ||
-            items.house.toLowerCase().includes(searchString)
-        );
-    });
-    displayCharacters(filteredItems);
-});
-
-const loadCharacters = async () => {
-    try {
-        const res = await fetch('https://hp-api.herokuapp.com/api/characters');
-        items = await res.json();
-        displayItems(items);
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-const displayItems = (items) => {
-    const htmlString = items
-        .map((items) => {
-            return `
-            <li class="items">
-                <h2>${items.name}</h2>
-                <p>House: ${items.house}</p>
-                <img src="${items.image}"></img>
-            </li>
-        `;
-        })
-        .join('');
-    itemsList.innerHTML = htmlString;
-};
-
-loadItems();
-*/
