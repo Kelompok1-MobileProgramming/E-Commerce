a6c97764ac8f5b0b55791c0ceb900dac57d50553
cynthianatalie535190034 <cynthia.535190034@stu.untar.ac.id>
2020-11-21 21:04:23 +0700

    21 Nov 21:05

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-//main page
-router.get('/', (req,res)=>{
-    res.render('Main');
-})
-//login page
-router.get('/signin', (req,res)=>{
-    res.render('signin');
-})
 
-//register page
-router.get('/register', (req,res)=>{
-    res.render('signinform');
-})
-
-//member page
-router.get('/member', (req,res)=>{
-    res.render('Member');
-})
-
-//performance page
-router.get('/bootstrap', (req,res)=>{
-    res.render('bootstrap');
-})
+router.get('/',
+    async (req, res) => {
+        res.send('ok');
+    });
 
 module.exports = router; 
--- public/routes/users.js
+++ public/routes/users.js
@@ -2,25 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 //login handle
-router.get('/main',(req,res)=>{
-    res.render('views/Main');
+router.get('/main',
+  async(req,res)=>{
+    res.render('../views/Main');
 })
 
 router.get('/signin', 
 async (req,res) => {
-  if (req.session.user) {
-    res.redirect('/member');
-  } else {
-    res.render('pages/signin');
-  }
+  res.render('../views/signin');
 })
 
-router.get('/member',(req,res)=>{
-    res.render('views/Member.ejs')
+router.get('/member',
+async (req,res)=>{
+    res.render('../views/Member');
     })
 
 router.get('/bootstrap',(req,res)=>{
-    res.render('views/bootstrap.ejs')
+    res.render('../views/bootstrap');
     })
 //Register handle
 router.post('/main',(req,res)=>{
@@ -29,21 +27,22 @@ router.post('/main',(req,res)=>{
 router.post('/signin',
 async (req, res) => {
   // get user input
-  const username = req.body.username;
+  const username = req.body.name;
   const password = req.body.password;
 
   //check username and password
   if (username === "admin" && password === "admin") {
-    // create session for user
-    req.session.user = "admin";
-
+  
     //login success and redirect to member area
-    res.redirect('/member');
+    res.redirect('../users/member');
   } else {
     //render the login page with error information
-    res.render('pages/signin', {error: 'Wrong username or password'});
+    res.render('../views/signin', {error: 'Wrong username or password'});
   }
-  
+})
+
+router.get('/register', (req,res)=>{
+  res.render('signinform');
 })
 
 router.post('/member',(req,res,next)=>{
