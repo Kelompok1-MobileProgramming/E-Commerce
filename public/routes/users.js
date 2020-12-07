166fe25936aebd2d1a8d7e6f75070635e85a03e8
cynthianatalie535190034 <cynthia.535190034@stu.untar.ac.id>
2020-12-07 16:58:31 +0700

    7 des 2020 16:59

--- public/routes/users.js
+++ public/routes/users.js
@@ -237,3 +237,139 @@ module.exports  = router;
 router.get('/desc',(req,res) => {
   res.render('Description');
 })
+
+//desc
+router.get('/airpods',
+  async(req,res)=>{
+    res.render('../desc/airpods');
+})
+
+router.get('/baju',
+  async(req,res)=>{
+    res.render('../desc/baju');
+})
+
+router.get('/beras',
+  async(req,res)=>{
+    res.render('../desc/beras');
+})
+
+router.get('/bunga',
+  async(req,res)=>{
+    res.render('../desc/bunga');
+})
+
+router.get('/chitato',
+  async(req,res)=>{
+    res.render('../desc/chitato');
+})
+
+router.get('/cookies',
+  async(req,res)=>{
+    res.render('../desc/cookies');
+})
+
+router.get('/ig',
+  async(req,res)=>{
+    res.render('../desc/ig');
+})
+
+router.get('/japota',
+  async(req,res)=>{
+    res.render('../desc/japota');
+})
+
+router.get('/jeams',
+  async(req,res)=>{
+    res.render('../desc/jeans');
+})
+
+router.get('/jolly',
+  async(req,res)=>{
+    res.render('../desc/jolly');
+})
+
+router.get('/kotak',
+  async(req,res)=>{
+    res.render('../desc/kotak');
+})
+
+router.get('/maechee',
+  async(req,res)=>{
+    res.render('../desc/maechee');
+})
+
+router.get('/masker',
+  async(req,res)=>{
+    res.render('../desc/masker');
+})
+
+router.get('/mobil',
+  async(req,res)=>{
+    res.render('../desc/mobil');
+})
+
+router.get('/momogi',
+  async(req,res)=>{
+    res.render('../desc/momogi');
+})
+
+router.get('/nescafe',
+  async(req,res)=>{
+    res.render('../desc/nescafe');
+})
+
+router.get('/parfum',
+  async(req,res)=>{
+    res.render('../desc/parfum');
+})
+
+router.get('/pensil',
+  async(req,res)=>{
+    res.render('../desc/pensil');
+})
+
+router.get('/potabi',
+  async(req,res)=>{
+    res.render('../desc/potabi');
+})
+
+router.get('/roti',
+  async(req,res)=>{
+    res.render('../desc/roti');
+})
+
+router.get('/royco',
+  async(req,res)=>{
+    res.render('../desc/royco');
+})
+
+router.get('/samsung',
+  async(req,res)=>{
+    res.render('../desc/samsung');
+})
+
+router.get('/spidol',
+  async(req,res)=>{
+    res.render('../desc/spidol');
+})
+
+router.get('/sunsilk',
+  async(req,res)=>{
+    res.render('../desc/sunsilk');
+})
+
+router.get('/tas',
+  async(req,res)=>{
+    res.render('../desc/tas');
+})
+
+router.get('/teh',
+  async(req,res)=>{
+    res.render('../desc/teh');
+})
+
+router.get('/tiketkucing',
+  async(req,res)=>{
+    res.render('../desc/tiketkucing');
+})
