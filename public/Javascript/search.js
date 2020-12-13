6bda19df91a317dbbaf38ae8e8e11ac938377533
cynthianatalie535190034 <cynthia.535190034@stu.untar.ac.id>
2020-12-13 20:16:09 +0700

    13 des 2020 20:17

--- public/Javascript/search.js
+++ public/Javascript/search.js
@@ -1,30 +1,32 @@
-function search_item() { 
-    let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('items'); 
+function search_item() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('items');
     ol = document.getElementById('list');
     let y = document.getElementsByClassName('noitems');
 
     if(input){
         document.querySelector('.product').style.display = 'block';
+        document.querySelector('.product').style.top = '0px';
+        document.getElementsByClassName('container')[0].remove();
     }
     else{
-        document.querySelector('.product').style.display = 'none';
+        location.reload();
     }
 
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else if (x[i].innerHTML.toLowerCase().includes(input)) { 
-            
-            x[i].style.display="list-item";                  
-        } 
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else if (x[i].innerHTML.toLowerCase().includes(input)) {
+
+            x[i].style.display="list-item";
+        }
         else{
 
         }
-        
-    } 
+
+    }
     for(e = 0; e < y.length; e++){
         y[e].style.display="none";
     }
